82c18e5c7e65f0b7804d7cc779e5c4a9485bd732
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2022-06-26 23:15:52 +0530

    .

---
+++ src/Sortingvisualiser/Sortingvisualiser.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import './Sortingvisualiser.css';
+
+export class Sortingvisualiser extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            array:[],
+        };
+    }
+
+    componentDidMount(){
+        this.resetarr();
+    }
+
+    resetarr(){
+        const array = [];
+        for(let i=0;i<100;i++){
+            array.push(randomintfrominterval(10,1000));
+
+        }
+        this.setState({array});
+    }
+
+    render(){
+        const {array} = this.state;
+
+        return (
+            <>
+            {array.map((value,idx) =>{
+                <div className="array-bar" key={idx}>
+                    {value}
+
+                </div>
+            })}
+            </>
+        );
+      
+    }
+}
+
+function randomintfrominterval(min,max) {
+    return Math.floor(Math.random() * (max-min+1) + min);   
+}
