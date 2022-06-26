517cb054b6be13ed674556adfc3cb7c4c8245438
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2022-06-27 00:28:51 +0530

    merge sort to be added

--- src/Sortingvisualiser/Sortingvisualiser.jsx
+++ src/Sortingvisualiser/Sortingvisualiser.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Sortingvisualiser.css';
+import *as Sortingalgorithms from '../Sortingalgorithms/Sortingalgorithms.js' 
 
 export default class Sortingvisualiser extends React.Component {
     constructor(props){
@@ -17,11 +18,19 @@ export default class Sortingvisualiser extends React.Component {
     resetarr(){
         const array = [];
         for(let i=0;i<300;i++){
-            array.push(randomintfrominterval(10,690));
+            array.push(randomintfrominterval(10,660));
 
         }
         this.setState({array});
     }
+    mergesort(){
+        const jssortedarray=this.state.array.slice().sort();
+        const sortedarray=Sortingalgorithms.mergesort(this.state.array);
+        console.log(arraysareequal(jssortedarray,sortedarray));
+    }
+    quicksort(){}
+    bubblesort(){}
+    heapsort(){}
 
     render(){
         const {array} = this.state;
@@ -35,6 +44,10 @@ export default class Sortingvisualiser extends React.Component {
                 </div> 
             ))}
             <button onClick={() => this.resetarr()}>Naya Array Generate kro</button>
+            <button onClick={() => this.mergesort()}>Merge Sort</button>
+            <button onClick={() => this.quicksort()}>Quick Sort</button>
+            <button onClick={() => this.heapsort()}>Heap Sort</button>
+            <button onClick={() => this.bubblesort()}>Bubble Sort</button>
             </div>
         );
       
@@ -43,4 +56,14 @@ export default class Sortingvisualiser extends React.Component {
 
 function randomintfrominterval(min,max) {
     return Math.floor(Math.random() * (max-min+1) + min);   
+}
+
+function arraysareequal(array1,array2){
+    if(array1.length!=array2.length) return false;
+    for(let i=0;i<array1.length;i++){
+        if(array1[i]!=array2[i]){
+            return false;
+        }
+    }
+    return true;
 }
