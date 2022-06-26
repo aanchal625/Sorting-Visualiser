a0403d7ae5174562df5e63c81718fba79bf55faa
aanchal625 <82753429+aanchal625@users.noreply.github.com>
2022-06-27 00:02:33 +0530

    array generator button added

--- src/Sortingvisualiser/Sortingvisualiser.jsx
+++ src/Sortingvisualiser/Sortingvisualiser.jsx
@@ -16,8 +16,8 @@ export default class Sortingvisualiser extends React.Component {
 
     resetarr(){
         const array = [];
-        for(let i=0;i<100;i++){
-            array.push(randomintfrominterval(10,1000));
+        for(let i=0;i<300;i++){
+            array.push(randomintfrominterval(10,690));
 
         }
         this.setState({array});
@@ -27,14 +27,15 @@ export default class Sortingvisualiser extends React.Component {
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array-container">
             {array.map((value,idx) =>(
-                <div className="array-bar" key={idx}>
-                    {value}
+                <div className="array-bar" key={idx} style={{height: `${value}px`}}>
+                    
 
-                </div>
+                </div> 
             ))}
-            </>
+            <button onClick={() => this.resetarr()}>Naya Array Generate kro</button>
+            </div>
         );
       
     }
